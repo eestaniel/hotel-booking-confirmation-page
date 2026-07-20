2e55f5434c91d18c61c74c0ed02a65c98d25bb96
EEstaniel <edwin@eestaniel.com>
2026-07-20 09:52:19 -0700

    feat(booking): add fanning card animation on hover

--- hotel-booking/src/components/BookingDocuments/BookingDocuments.tsx
+++ hotel-booking/src/components/BookingDocuments/BookingDocuments.tsx
@@ -2,12 +2,16 @@ import { WelcomeCard } from '../WelcomeCard/WelcomeCard'
 import { StayReceipt } from '../StayReceipt/StayReceipt'
 import styles from './BookingDocuments.module.css'
 import iconSparkleUrl from '../../assets/images/icon-sparkle.svg'
+import illustrationSunUrl from '../../assets/images/illustration-sun.svg'
 
 export const BookingDocuments = () => {
     return (
         <section className={styles.section} aria-label='Booking documents'>
             <div className={styles.container}>
                 <StayReceipt />
+                <div className={styles.sunWrapper} aria-hidden='true'>
+                    <img src={illustrationSunUrl} alt='' className={styles.sunIllustration} />
+                </div>
                 <WelcomeCard />
             </div>
             <div className={styles.hint}>
