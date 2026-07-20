c5786dd4c0d3f2cd33d816f75088f0ca24d9623e
EEstaniel <edwin@eestaniel.com>
2026-07-20 00:12:14 -0700

    initial commit

---
+++ hotel-booking/src/App.tsx
@@ -0,0 +1,12 @@
+import { Home } from './pages/Home'
+import styles from './styles/App.module.css'
+
+export const App = () => {
+    return (
+        <main className={styles.main}>
+            <Home />
+        </main>
+    )
+}
+
+export default App
